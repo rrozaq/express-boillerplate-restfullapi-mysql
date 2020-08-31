ca6cf3ef41ae9d2a79f62bf78794d837c9fb458e
Khoirur Rozaq <rozaqarmedian@gmail.com>
2020-08-31 12:36:48 +0700

    verifyToken jwt route

--- routes/siswa.js
+++ routes/siswa.js
@@ -4,11 +4,12 @@ const express = require('express');
 const siswaController = require('../controllers/siswa/siswaController');
 const { API_PATH } = require('../helpers/constant');
 const router = express.Router();
+const verifyToken = require('../helpers/token_validation');
 
-router.get(`${API_PATH}/siswa`, siswaController.index);
-router.get(`${API_PATH}/siswa/:id`, siswaController.show);
-router.post(`${API_PATH}/siswa`, siswaController.store);
-router.put(`${API_PATH}/siswa/:id`, siswaController.update);
-router.delete(`${API_PATH}/siswa/:id`, siswaController.delete);
+router.get(`${API_PATH}/siswa`, verifyToken, siswaController.index);
+router.get(`${API_PATH}/siswa/:id`, verifyToken, siswaController.show);
+router.post(`${API_PATH}/siswa`, verifyToken, siswaController.store);
+router.put(`${API_PATH}/siswa/:id`, verifyToken, siswaController.update);
+router.delete(`${API_PATH}/siswa/:id`, verifyToken, siswaController.delete);
 
 module.exports = router;
